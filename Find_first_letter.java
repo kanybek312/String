62171541c72ee148ca7153114038c78ebd2dee6d
Kanybek <m.kanybek@mail.ru>
2020-06-17 16:46:42 +0200

    String

---
+++ Find_first_letter.java
@@ -0,0 +1,15 @@
+package June;
+import  java.util.Scanner;
+
+public class Find_first_letter {
+    public static  void main(String arg[]){
+        Scanner sc =  new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        if (ch == 'J' | ch == 'j'){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+    }
+}
