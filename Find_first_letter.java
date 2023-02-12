7a9a6c35208804a6313a257eb122a6f57b5c7547
Kanybek Mukalaev <m.kanybek@mail.ru>
2023-02-12 12:40:36 +0100

    afa

--- Find_first_letter.java
+++ Find_first_letter.java
@@ -5,7 +5,7 @@ public class Find_first_letter {
     public static  void main(String arg[]){
         Scanner sc =  new Scanner(System.in);
         char ch = sc.next().charAt(0);
-        if (ch == 'A' | ch == 'j'){
+        if (ch == 'C' | ch == 'j'){
             System.out.println("true");
         } else {
             System.out.println("false");
